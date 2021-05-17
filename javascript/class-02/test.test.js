d6f0cdf6028f9f447f17d54d6eaa5d47583b9906
wafaankoush99 <wafadirawe@gmail.com>
2021-05-18 01:07:59 +0300

    mod

--- javascript/class-02/test.test.js
+++ javascript/class-02/test.test.js
@@ -1,16 +1,21 @@
-function insertShiftArray(arr, value){
+function insertShiftArray(arr, value) {
 
-  let newarr=[]
-  for (let i=0; i<(arr.length/2) ;i++){
-    newarr.push(arr[i])
+  let newArr = [];
+  for (let i = 0; i < (arr.length / 2); i++) {
+    newArr.push(arr[i]);
   }
-  newarr.push(value)
-    for (var i = arr.length-1; i >= (arr.length/2); i--){
-    newarr.push(arr[i])
+  newArr.push(value);
+  for (var i = 0; i <= (arr.length / 2) - 1; i++) {
+    let index = i + (arr.length / 2);
+    newArr.push(arr[Math.ceil(index)]);
   }
-  console.log(newarr)
+  return newArr;
 
 }
 
-insertShiftArray([2,4,6,-8], 5);
-insertShiftArray([42,8,15,23,42], 16);
+describe('Testing challenge', () => {
+  test('It should return reversed array', () => {
+    expect(insertShiftArray([2, 4, 6, -8], 5)).toStrictEqual([2,4,5,6,-8]);
+    expect(insertShiftArray([42, 8, 15, 23, 42], 16)).toStrictEqual([42,8,15,16,23,42]);
+  });
+});
